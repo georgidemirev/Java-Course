36f4d8e45254f546d27e6d1aa1c46924dbeda78b
Georgi Demirev <georgidemirev@users.noreply.github.com>
2019-11-15 19:26:17 +0200

    update

--- IssueTracker/src/bg/sofia/uni/fmi/mjt/jira/enums/IssueStatus.java
+++
@@ -1,5 +0,0 @@
-package bg.sofia.uni.fmi.mjt.jira.enums;
-
-public enum IssueStatus {
-    OPEN, IN_PROGRESS, RESOLVED
-}
--- IssueTracker/src/bg/sofia/uni/fmi/mjt/jira/interfaces/Filter.java
+++
@@ -1,7 +0,0 @@
-package bg.sofia.uni.fmi.mjt.jira.interfaces;
-
-import bg.sofia.uni.fmi.mjt.jira.issues.Issue;
-
-public interface Filter {
-    public Issue find(String issueID);
-}
