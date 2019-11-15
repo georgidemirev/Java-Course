fc45754ae9885f2bdbe7013a266c54c271225e2a
Georgi Demirev <georgidemirev@users.noreply.github.com>
2019-11-16 00:05:42 +0200

    rework

--- IssueTracker/src/bg/sofia/uni/fmi/mjt/jira/enums/IssueResolution.java
+++
@@ -1,5 +0,0 @@
-package bg.sofia.uni.fmi.mjt.jira.enums;
-
-public enum IssueResolution {
-    FIXED, WONT_FIX, DUPLICATE, CANNOT_REPRODUCE, UNRESOLVED
-}
--- IssueTracker/src/bg/sofia/uni/fmi/mjt/jira/interfaces/Repository.java
+++
@@ -1,7 +0,0 @@
-package bg.sofia.uni.fmi.mjt.jira.interfaces;
-
-import bg.sofia.uni.fmi.mjt.jira.issues.Issue;
-
-public interface Repository {
-    public void addIssue(Issue issue);
-}
