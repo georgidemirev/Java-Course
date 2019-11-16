f5068143a77eac9d2142e4cbc27ec2a0a1c1c20b
Georgi Demirev <georgidemirev@users.noreply.github.com>
2019-11-16 09:44:10 +0200

    test

---
+++ ShoppingCart/tests/bg/sofia/uni/fmi/mjt/shopping/ShoppingCartTest.java
@@ -0,0 +1,27 @@
+package bg.sofia.uni.fmi.mjt.shopping;
+
+import static org.junit.Assert.*;
+
+public class ShoppingCartTest {
+
+    @org.junit.Test
+    public void getUniqueItems() {
+    }
+
+    @org.junit.Test
+    public void getSortedItems() {
+    }
+
+    @org.junit.Test
+    public void addItem() {
+    }
+
+    @org.junit.Test
+    public void removeItem() {
+    }
+
+    @org.junit.Test
+    public void getTotal() {
+    }
+
+}
---
+++ ShoppingCart/tests/bg/sofia/uni/fmi/mjt/shopping/item/ItemTest.java
@@ -0,0 +1,4 @@
+package bg.sofia.uni.fmi.mjt.shopping.item;
+
+public class ItemTest {
+}
