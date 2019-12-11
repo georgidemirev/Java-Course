d9ae55609857fc0ba38e97db658750d58f062325
Georgi Demirev <georgidemirev@users.noreply.github.com>
2019-12-12 00:05:53 +0200

    project upload

--- Week7/StyleChecker/src/bg/sofia/uni/fmi/mjt/stylechecker/StyleChecker.java
+++ Week7/StyleChecker/src/bg/sofia/uni/fmi/mjt/stylechecker/StyleChecker.java
@@ -1,9 +1,6 @@
 package bg.sofia.uni.fmi.mjt.stylechecker;
 
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.Reader;
-import java.io.Writer;
+import java.io.*;
 
 /**
  * Checks adherence to Java style guidelines.
@@ -11,18 +8,9 @@ import java.io.Writer;
 
 public class StyleChecker {
 
-    public StyleChecker() {
-    }
+    // make Singleton !
 
-    public static void main(String[] args) throws IOException {
-        //FileWriter p = new FileWriter("test/bg/sofia/uni/fmi/mjt/stylechecker/" + "test.txt");
-
-        //p.close();
-        int i = 100;
-        i = --i + 1;
-        System.out.println(i);
-    }
-
+    private static final int MAX_LENGTH_OF_LINE = 100;
     // FIXME Wildcards are not allowed in import statements
     // FIXME Only one statement per line is allowed
     // FIXME Length of line should not exceed 100 characters
@@ -40,9 +28,81 @@ public class StyleChecker {
      */
     public void checkStyle(Reader source, Writer output) throws IOException {
         BufferedWriter writer = new BufferedWriter(output);
-        writer.write("wtf");
-        writer.close();
-        // implementation
+        BufferedReader reader = new BufferedReader(source);
+
+        String line = reader.readLine();
+        while (line != null) {
+            if (line.length() > 0) {
+                checkAllRules(line, output);
+                output.write(line + "\n");
+            }
+            line = reader.readLine();
+        }
     }
 
+    private void checkAllRules(String line, Writer output) throws IOException {
+        checkIfOnlyOneStatements(line, output);
+        checkWildcardsInImports(line, output);
+        checkBrackets(line, output);
+        checkPackageUppercaseAnd_(line, output);
+        checkLength(line, output);
+
+    }
+
+    private void checkIfOnlyOneStatements(String line, Writer output) throws IOException {
+        String[] separated = line.split(";");
+        if (separated.length > 1) {
+            output.write("// FIXME Only one statement per line is allowed\n");
+        }
+    }
+
+    private void checkWildcardsInImports(String line, Writer output) throws IOException {
+        if (line.contains("import") && line.contains("*")) {
+            output.write("// FIXME Wildcards are not allowed in import statements\n");
+        }
+    }
+
+    private void checkBrackets(String line, Writer output) throws IOException {
+        if (line.charAt(0) == '{') {
+            output.write("// FIXME Opening brackets should be " +
+                    "placed on the same line as the declaration\n");
+        }
+    }
+
+    private void checkPackageUppercaseAnd_(String line, Writer output) throws IOException {
+        if (line.contains("_")) {
+            output.write("// FIXME Package name should not " +
+                    "contain upper-case letters or underscores\n");
+            return;
+        }
+        if (line.contains("package")) {
+            String startingWith = line.substring(0, 6);
+            if (startingWith.equals("package")) {
+                for (int i = 7; i < line.length(); i++) {
+                    char p = line.charAt(i);
+                    if (Character.isUpperCase(p)) {
+                        output.write("// FIXME Package name should not " +
+                                "contain upper-case letters or underscores\n");
+                    }
+                }
+            }
+        }
+    }
+
+    private void checkLength(String line, Writer output) throws IOException {
+        int i = 0;
+        int size = 0;
+        while (i < line.length()) {
+            if (line.charAt(i) != ' ') {
+                size = i + 1;
+            }
+            i++;
+        }
+        if (size > MAX_LENGTH_OF_LINE) {
+            if (!line.contains("import")) {
+                output.write("// FIXME Length of line" +
+                        " should not exceed 100 characters\n");
+            }
+        }
+    }
 }
--- Week7/StyleChecker/test/bg/sofia/uni/fmi/mjt/stylechecker/StyleCheckerTest.java
+++ Week7/StyleChecker/test/bg/sofia/uni/fmi/mjt/stylechecker/StyleCheckerTest.java
@@ -1,5 +1,6 @@
 package bg.sofia.uni.fmi.mjt.stylechecker;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 import java.io.*;
@@ -7,10 +8,78 @@ import java.io.*;
 public class StyleCheckerTest {
 
     @Test
-    public void test() throws IOException {
-        StyleChecker styleChecker = new StyleChecker();
-        Writer writer = new FileWriter("test/bg/sofia/uni/fmi/mjt/stylechecker/" + "testOutput.txt");
-        Reader reader = new FileReader("test/bg/sofia/uni/fmi/mjt/stylechecker/" + "test.txt");
-        styleChecker.checkStyle(reader, writer);
+    public void TestIfOnlyOneStatements() throws IOException {
+
+        Reader input = new StringReader("import java.util;a;");
+        Writer output = new StringWriter();
+
+        StyleChecker checker = new StyleChecker();
+        checker.checkStyle(input, output);
+
+        Assert.assertEquals("// FIXME Only one statement per line is allowed\n"
+                + "import java.util;a;\n", output.toString());
+    }
+
+    @Test
+    public void testWildcardsInImports() throws IOException {
+
+        Reader input = new StringReader("import java.util.*;");
+        Writer output = new StringWriter();
+
+        StyleChecker checker = new StyleChecker();
+        checker.checkStyle(input, output);
+        String actual = output.toString();
+
+        Assert.assertEquals("// FIXME Wildcards are not allowed in import statements\n" +
+               "import java.util.*;", actual.strip());
+    }
+
+    @Test
+    public void testBrackets() throws IOException {
+
+        Reader input = new StringReader("import java.util.;\n" +
+                "{");
+        Writer output = new StringWriter();
+
+        StyleChecker checker = new StyleChecker();
+        checker.checkStyle(input, output);
+        String actual = output.toString();
+
+        Assert.assertEquals("import java.util.;\n" +
+                "// FIXME Opening brackets should be " +
+                "placed on the same line as the declaration\n" +
+                "{", actual.strip());
+    }
+
+    @Test
+    public void testPackageUppercaseAnd_() throws IOException {
+
+        Reader input = new StringReader("package bg.uni_sofia.fmi.mjt;");
+        Writer output = new StringWriter();
+
+        StyleChecker checker = new StyleChecker();
+        checker.checkStyle(input, output);
+        String actual = output.toString();
+
+        Assert.assertEquals("// FIXME Package name should not contain " +
+                "upper-case letters or underscores\n" +
+                "package bg.uni_sofia.fmi.mjt;", actual.strip());
+    }
+
+    @Test
+    public void testLength() throws IOException {
+
+        Reader input = new StringReader("java.util.asggdggggggggggggg" +
+                "ggggggggggggggggggggggggggadgggggggggggggggggggggggggg" +
+                "gggggggggggggggggggggggggggggggggg;");
+        Writer output = new StringWriter();
+
+        StyleChecker checker = new StyleChecker();
+        checker.checkStyle(input, output);
+        String actual = output.toString();
+
+        Assert.assertEquals("// FIXME Length of line should not exceed 100 characters\n" +
+                "java.util.asggdgggggggggggggggggggggggggggggggggggggggadg" +
+                "ggggggggggggggggggggggggggggggggggggggggggggggggggggggggggg;", actual.strip());
     }
 }
